e893175e8e829db66b5b212f3ea63a914ca7a10d
Radhep <Radhep.Saba@gmail.com>
2018-01-14 21:39:53 -0500

    Added all text required for the story as String resources

--- app/src/main/java/com/radhep/interactivestory/model/Page.java
+++ app/src/main/java/com/radhep/interactivestory/model/Page.java
@@ -6,4 +6,20 @@ public class Page {
 
     private int imageId;
     private int textId;
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
 }
